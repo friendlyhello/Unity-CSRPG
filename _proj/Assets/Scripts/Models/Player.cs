abb19b7012695bbb0601b7b573adf252213d3de0
Val Nuñez <val@friendlyhello.com>
2021-12-13 22:20:43 -0800

    Setting up the player class

---
+++ _proj/Assets/Scripts/Models/Player.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player 
+{
+    // Properties
+    public string Name { get; set; }
+    public string CharacterClass { get; set; }
+    public int HitPoints { get; set; }
+    public int Gold { get; set; }
+    public int ExperiencePoints { get; set; }
+    public int Level { get; set; }
+}
